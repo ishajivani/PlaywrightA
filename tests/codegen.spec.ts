e7bab81bf54c6370d71efd92001f3da053295d9b
ishajivani <ishajivani@gmail.com>
2026-06-11 17:21:05 +0530

    commited

--- tests/codegen.spec.ts
+++ tests/codegen.spec.ts
@@ -1,12 +1,18 @@
 import { test, expect } from '@playwright/test';
 
 test('Codegen Recording', async ({ page }) => {
-await page.goto('https://www.amazon.in/');
-await page.getByRole('searchbox', { name: 'Search Amazon.in' }).click();
-await page.getByRole('searchbox', { name: 'Search Amazon.in' }).fill('iphone17');
-await page.getByRole('searchbox', { name: 'Search Amazon.in' }).press('Enter');
-await page.locator('#a-autoid-1-announce').click();
-await page.getByRole('link', { name: 'item in cart' }).click();
-await page.getByRole('button', { name: 'Increase quantity by one' }).click();
-await page.getByRole('button', { name: 'Proceed to Buy Buy Amazon' }).click();
+await page.goto('https://phptravels.net/');
+await page.getByRole('button', { name: 'I Understand & Continue' }).click();
+await page.getByRole('tab', { name: 'mosque Umrah' }).click();
+await page.getByText('location_on Destination Select City expand_more').click();
+await page.getByText('location_city Makkah Saudi').click();
+await page.getByRole('button', { name: 'Search Umrah' }).click();
+await page.getByRole('link', { name: 'info More Details' }).first().click();
+await page.getByRole('button', { name: 'Arrival in Makkah' }).click();
+await page.getByRole('button', { name: 'Performing Umrah' }).click();
+await page.getByRole('button', { name: 'Discovering Makkah\'s Heritage' }).click();
+await page.getByRole('button', { name: 'Visit to Madinah' }).click();
+await page.getByRole('button', { name: 'Exploring Islamic Sites' }).click();
+await page.getByRole('button', { name: 'Final Days of Worship' }).click();
+await page.getByRole('button', { name: 'Departure' }).click();
 });
