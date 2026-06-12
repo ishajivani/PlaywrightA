69a198c8570a9744fb1d71e825a7b8dcac41963b
ishajivani <ishajivani@gmail.com>
2026-06-12 16:06:10 +0530

    commited

--- tests/Holiday.spec.ts
+++ tests/Holiday.spec.ts
@@ -2,14 +2,13 @@ import { test } from '@playwright/test';
 import { HomePage } from '../pages/holidayhome';
 import { SearchResultsPage } from '../pages/holidaySearch';
 
-// test('Search holiday package', async ({ page }) => {
-//     const homePage = new HomePage(page);
-//     const searchResultsPage = new SearchResultsPage(page);
-//     await homePage.navigateToHomePage();
-//     await homePage.searchTrip();
-//     await searchResultsPage.closePopupIfPresent();
-//     await searchResultsPage.clickPriceRangeFilter();
-// });
+test('Search holiday package', async ({ page }) => {
+    const homePage = new HomePage(page);
+    const searchResultsPage = new SearchResultsPage(page);
+    await homePage.navigateToHomePage();
+    await homePage.searchTrip();
+    await searchResultsPage.closePopupIfPresent();
+});
 
 test('Without Filters Flow', async ({page}) => { 
     const hoamepage = new HomePage(page);
@@ -36,4 +35,3 @@ test('Uttarakhand Package Flow', async ({ page }) => {
     await searchResultsPage.openPackage();
     await searchResultsPage.verifyPackageTabs();
 });
-
