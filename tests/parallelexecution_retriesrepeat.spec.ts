b54948cff37c1b81f5c02fe3369ed1ffd8e1e38c
ishajivani <ishajivani@gmail.com>
2026-06-15 18:50:02 +0530

    commited

--- tests/parallelexecution_retriesrepeat.spec.ts
+++ tests/parallelexecution_retriesrepeat.spec.ts
@@ -1,32 +1,10 @@
 import { test, expect } from '@playwright/test';
 
-// test.describe.configure({
-//   mode: 'parallel'
-// });
+test.describe.configure({
+  mode: 'parallel'
+});
 
-// test('Mobile Search', async ({ page }) => {
-//   await page.waitForTimeout(5000);
-//   await page.goto('https://www.amazon.in/');
-//   await page.waitForTimeout(5000);
-//   await page.getByRole('searchbox', { name: 'Search Amazon.in' }).click();
-//   await page.waitForTimeout(5000);
-//   await page.getByRole('searchbox', { name: 'Search Amazon.in' }).fill('mobile');
-//   await page.waitForTimeout(5000);
-//   await page.keyboard.press('Enter');
-//   await page.waitForTimeout(5000);
-//   await page.getByRole('button', { name: 'Add to cart' }).first().click();
-//   await page.waitForTimeout(5000);
-// });
-
-// test('Boat Search', async ({ page }) => {
-//     await page.goto('https://www.amazon.in/');
-//     await page.getByRole('searchbox', { name: 'Search Amazon.in' }).click();
-//     await page.getByRole('searchbox', { name: 'Search Amazon.in' }).fill('boat');
-//     await page.keyboard.press('Enter');
-//     await page.locator('#a-autoid-1-announce').click();
-//     await page.waitForTimeout(5000);
-// });
-
+// useage of only one worker as in parallel mode if 2 workeres work together there is clash in database 
 
 test('Mobile Search', async ({ page }) => {
   await page.goto('https://www.amazon.in/');
